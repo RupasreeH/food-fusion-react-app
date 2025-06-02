d726b12c458154dbd56433d14e52388022fee674
RupasreeH <hroopa015@gmail.com>
2025-06-02 17:11:16 -0500

    rp - committed for project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+Head - Logo - Nav Items
+Body - Search - RestContainer - RestoCards - Img - Name of res, Star rating, cusinsines, delivery tie
+Footer - CopyRight - Links - Address - Contract
---
+++ src/components/Body.js
@@ -0,0 +1,26 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("button clicked");
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
