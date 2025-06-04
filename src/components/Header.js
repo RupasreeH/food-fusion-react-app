03f9f3619acc6791353b53bb30be944ef757979c
RupasreeH <hroopa015@gmail.com>
2025-06-04 17:04:05 -0500

    rp - committed for fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
   console.log("header");
+  useEffect(() => {
+    console.log("useEffect called");
+  }, []);
   return (
     <div className="header">
       <div className="logo-container">
